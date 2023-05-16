d896914d73b68d6d9f888072925b5a2c04b39c91
Aashish Shrestha <saashish7070@gmail.com>
2023-05-16 10:59:11 +0545

    The extension is ready to use

---
+++ popup.js
@@ -0,0 +1,48 @@
+// Get the links from the text file
+fetch(chrome.runtime.getURL("leetcode_links.txt"))
+  .then((response) => response.text())
+  .then((text) => {
+    const links = text.split("\n").map((link) => link.trim());
+    const visitedLinks = JSON.parse(localStorage.getItem("visitedLinks") || "[]");
+
+    // Render the links
+    const linksDiv = document.getElementById("links");
+    links.forEach((link) => {
+      const linkDiv = document.createElement("div");
+      linkDiv.className = "link";
+      linkDiv.innerText = link;
+      if (visitedLinks.includes(link)) {
+        linkDiv.classList.add("visited");
+      }
+      linkDiv.addEventListener("click", () => {
+        if (!visitedLinks.includes(link)) {
+          visitedLinks.push(link);
+          localStorage.setItem("visitedLinks", JSON.stringify(visitedLinks));
+          linkDiv.classList.add("visited");
+        }
+        chrome.tabs.create({ url: link, active: false }, () => {
+          // Remove the link from the UI when it's opened
+          linkDiv.remove();
+        });
+      });
+      linksDiv.appendChild(linkDiv);
+    });
+  });
+
+// Reset the visited links
+document.getElementById("reset").addEventListener("click", () => {
+  localStorage.removeItem("visitedLinks");
+  document.querySelectorAll(".link.visited").forEach((linkDiv) => {
+    linkDiv.classList.remove("visited");
+  });
+});
+// Reset the visited links
+// document.getElementById("reset").addEventListener("click", () => {
+//   console.log("Reset button clicked");
+//   localStorage.removeItem("visitedLinks");
+//   console.log("Visited links cleared from localStorage");
+//   document.querySelectorAll(".link.visited").forEach((linkDiv) => {
+//     linkDiv.classList.remove("visited");
+//     console.log("Visited link cleared from UI");
+//   });
+// });
